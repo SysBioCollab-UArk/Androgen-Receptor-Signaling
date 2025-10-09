6a7535aba042cdf6cd098f4c43c36a66e7dbced1
najahb <nabiggs@uark.edu>
2025-10-08 22:42:50 -0400

    Added all of the monomers

--- AR_model.py
+++ AR_model.py
@@ -5,8 +5,71 @@ import matplotlib.pyplot as plt
 Model()
 
 # monomers
+
+# Ligands & Receptors
 Monomer('EGF',['r'])
 Monomer('EGFR',['l','d','state'],{'state':['u','p']})
+
+# Androgen axis (Ligand + receptor + co-regulators)
+Monomer('DHT', ['r'])
+Monomer('AR', ['l'])
+Monomer('PGC1A', ['b'])
+Monomer('ESRRA', ['b'])
+Monomer('NRF1', ['b'])
+Monomer('TFAM', ['b'])
+
+# PI3K-AKT axis and crosstalk
+Monomer('PI3K', ['b'])
+Monomer('PIP3', ['b'])
+Monomer('AKT', ['b'])
+Monomer('PTEN', ['b'])
+Monomer('mTOR', ['b'])
+Monomer('FOXO', ['b'])
+Monomer('GSK3B', ['b'])
+Monomer('BAD', ['b'])
+
+# MAPK axis (optional downstream of EGFR/RAS)
+Monomer('GRB2', ['b'])
+Monomer('SOS', ['b'])
+Monomer('RAS', ['b'])
+Monomer('RAF', ['b'])
+Monomer('MEK', ['b'])
+Monomer('ERK', ['b'])
+
+# Mitochondrial dynamics & quality control
+Monomer('DRP1', ['b'])
+Monomer('MFN1', ['b'])
+Monomer('MFN2', ['b'])
+Monomer('OPA1', ['b'])
+Monomer('PINK1', ['b'])
+Monomer('PRKN', ['b'])
+Monomer('ROS', ['b'])
+
+# Mitochondrial UPR (UPRmt)
+Monomer('HSPD1', ['b'])
+Monomer('HSPE1', ['b'])
+Monomer('HSPA9', ['b'])
+Monomer('LONP1', ['b'])
+Monomer('CLPP', ['b'])
+Monomer('ATF5', ['b'])
+
+# Apoptosis (intrinsic pathway)
+Monomer('CYCS', ['b'])
+Monomer('APAF1', ['b'])
+Monomer('CASP9', ['b'])
+Monomer('CASP3', ['b'])
+Monomer('XIAP', ['b'])
+Monomer('SMAC', ['b'])
+
+# BCL-2 family (anti-apoptotic, effectors, BH3-only)
+Monomer('BCL2', ['bh3'])
+Monomer('BCLXL', ['bh3'])
+Monomer('MCL1', ['bh3'])
+Monomer('BAX', ['b'])
+Monomer('BAK', ['b'])
+Monomer('BIM', ['bh3'])
+Monomer('PUMA', ['bh3'])
+Monomer('NOXA', ['bh3'])
 
 
 # initial concentrations
