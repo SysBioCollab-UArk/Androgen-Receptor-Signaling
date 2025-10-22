30743447830c8836d971528aaf6dbe865f532aea
najahb <nabiggs@uark.edu>
2025-10-22 16:04:19 -0400

    Fixed erros from pervious version still writing rules

--- AR_model.py
+++ AR_model.py
@@ -8,15 +8,16 @@ Model()
 
 # Ligands & Receptors
 Monomer('EGF', ['r'])
-Monomer('EGFR', ['l','d','grb2_shc','sos','pi3k','erk','state'],{'state':['u','p']})
-Monomer('Her2', ['d','grb2_shc','sos','pi3k','state'],{'state':['u','p']})
+Monomer('EGFR', ['l','d','grb2_shc','state'],{'state':['u','p']})
+Monomer('Her2', ['d','grb2_shc','cpacp','state'],{'state':['u','p']})
 Monomer('Grb2', ['sos','shc','egfr_her2'])
-Monomer('Sos', ['grb2','ras'])
-Monomer('Ras', ['sos','gap','raf','state'],{'state':['GDP','GTP']})
 Monomer('Shc', ['egfr_her2','grb2','state'],{'state':['u','p']})
+Monomer('Sos', ['grb2','ras_erk','pi3k'])
+Monomer('Ras', ['sos','gap','raf','state'],{'state':['GDP','GTP']})
+
 
 # 2) Phosphatases acting on HER2/AR modules
-Monomer('cPAcP',  ['her2_olig']) #dimer
+Monomer('cPAcP',  ['her2']) #dimer
 Monomer('sPAcP',  ['her2'])
 
 # 3) RAS–RAF–MEK–ERK cascade
@@ -25,7 +26,7 @@ Monomer('Raf',    ['ras','mek','pase1','state'],{'state':['u','p']})
 Monomer('Pase1',  ['raf'])
 Monomer('MEK',    ['raf','erk','pase2','state'],{'state':['u','p','pp']})
 Monomer('Pase2',  ['mek'])
-Monomer('ERK',    ['mek','pase3','egfr_her2','ets','ap1','state'],{'state':['u','p','pp']})
+Monomer('ERK',    ['mek','pase3','sos','ets','ap1','state'],{'state':['u','p','pp']})
 Monomer('Pase3',  ['erk'])
 
 # 4) ERK nuclear targets
@@ -34,10 +35,10 @@ Monomer('AP1',    ['erk','rnap','state'],{'state':['u','p']})
 
 # 5) Internalization forms used explicitly in reactions
 Monomer('EGFRi',  ['l','d','grb2','sos','shc','erk'])
-Monomer('EGFi',   [])
+Monomer('EGFi')
 
 # 6) PI3K–AKT–mTOR arm
-Monomer('PI3K',   ['egfr_her2','ptdins2','state'],{'state':['i','act']})
+Monomer('PI3K',   ['egfr_her2','ptdins2','sos','state'],{'state':['i','act']})
 Monomer('PtdIns2',['pi3k'])
 Monomer('PtdIns3',['pten','akt','pdk1'])
 Monomer('PTEN',   ['ptdins3'])
@@ -49,9 +50,9 @@ Monomer('eIF4E',  ['e4ebp1','mrna'])
 Monomer('Pase7',  ['akt'])
 
 # 7) Transport/secreted forms used explicitly
-Monomer('T_e',    [])
+Monomer('T_e')
 Monomer('T',      ['rase5a','ar'])
-Monomer('sPAcP_e',[])
+Monomer('sPAcP_e')
 
 # 8) AR axis and modifiers
 Monomer('Rase5a', ['T'])
@@ -77,8 +78,8 @@ Monomer('S40', ['mrna'])
 Monomer('S60', ['mrna'])
 
 # 11) Protein products (sinks in your list)
-Monomer('CycD', [])
-Monomer('PSA', [])
+Monomer('CycD')
+Monomer('PSA')
 
 
 
@@ -87,18 +88,21 @@ Parameter('Lig_0', 100)
 Parameter('Rec_0', 100)
 
 Initial(EGF(r=None), Lig_0)
-Initial(EGFR(l=None, d=None, state='u'), Rec_0)
+Initial(EGFR(l=None, d=None,grb2_shc=None, state='u'), Rec_0)
 
 # rules
 Parameter('kf_LR', 1)
 Parameter('kr_LR', 1)
 Rule('lig_binds_rec', EGF(r=None) + EGFR(l=None, d=None) | EGF(r=1) % EGFR(l=1, d=None), kf_LR, kr_LR)
-Rule('Her2p_binds_cPAcP',Her2(d=ANY, state='p') + cPAcP(her2_olig=None) |
-Her2(d=1, state='p') % cPAcP(her2_olig=1),
+
+Parameter('kf_Her2_cPAcP', 1)
+Parameter('kr_Her2_cPAcP', 1)
+Rule('Her2p_binds_cPAcP',Her2(d=ANY, cpacp=None,state='p') + cPAcP(her2=None) |
+Her2(d=ANY, cpacp=1, state='p') % cPAcP(her2=1),
 kf_Her2_cPAcP, kr_Her2_cPAcP)
 
 
-# observables
+# observable
 Observable('Lig_free', EGF(r=None))
 Observable('Lig_bound', EGF(r=ANY))
 Observable('Rec_unphos', EGFR(state='u'))
