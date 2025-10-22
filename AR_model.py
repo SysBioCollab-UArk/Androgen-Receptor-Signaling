811934f03974fdf5d2f5172443023389ff86474c
najahb <nabiggs@uark.edu>
2025-10-21 22:00:15 -0400

    fixing monomers and added a new rule. Exit code 1 need to fix the rules I believe the monomers are finally correct

--- AR_model.py
+++ AR_model.py
@@ -7,71 +7,79 @@ Model()
 # monomers
 
 # Ligands & Receptors
-Monomer('EGF',['r'])
-Monomer('EGFR',['l','d','grb2_shc','state'],{'state':['u','p']})
-Monomer('Her2', ['d','grb2_shc','state'],{'state':['u','p']})
+Monomer('EGF', ['r'])
+Monomer('EGFR', ['l','d','grb2_shc','sos','pi3k','erk','state'],{'state':['u','p']})
+Monomer('Her2', ['d','grb2_shc','sos','pi3k','state'],{'state':['u','p']})
+Monomer('Grb2', ['sos','shc','egfr_her2'])
+Monomer('Sos', ['grb2','ras'])
+Monomer('Ras', ['sos','gap','raf','state'],{'state':['GDP','GTP']})
+Monomer('Shc', ['egfr_her2','grb2','state'],{'state':['u','p']})
 
-# Androgen axis (Ligand + receptor + co-regulators)
-Monomer('DHT', ['r'])
-Monomer('AR', ['l'])
-Monomer('PGC1A', ['b'])
-Monomer('ESRRA', ['b'])
-Monomer('NRF1', ['b'])
-Monomer('TFAM', ['b'])
+# 2) Phosphatases acting on HER2/AR modules
+Monomer('cPAcP',  ['her2_olig']) #dimer
+Monomer('sPAcP',  ['her2'])
 
-# PI3K-AKT axis and crosstalk
-Monomer('PI3K', ['b'])
-Monomer('PIP3', ['b'])
-Monomer('AKT', ['b'])
-Monomer('PTEN', ['b'])
-Monomer('mTOR', ['b'])
-Monomer('FOXO', ['b'])
-Monomer('GSK3B', ['b'])
-Monomer('BAD', ['b'])
+# 3) RAS–RAF–MEK–ERK cascade
+Monomer('GAP',    ['ras'])
+Monomer('Raf',    ['ras','mek','pase1','state'],{'state':['u','p']})
+Monomer('Pase1',  ['raf'])
+Monomer('MEK',    ['raf','erk','pase2','state'],{'state':['u','p','pp']})
+Monomer('Pase2',  ['mek'])
+Monomer('ERK',    ['mek','pase3','egfr_her2','ets','ap1','state'],{'state':['u','p','pp']})
+Monomer('Pase3',  ['erk'])
 
-# MAPK axis (optional downstream of EGFR/RAS)
-Monomer('GRB2', ['r','r','sos','shc'])
-Monomer('SOS', ['grb2','ras'])
-Monomer('RAS', ['sos','state'],{'state':['GDP','GTP']})
-Monomer('Shc', ['r','r','grb2'])
-Monomer('RAF', ['b'])
-Monomer('MEK', ['b'])
-Monomer('ERK', ['b'])
+# 4) ERK nuclear targets
+Monomer('ETS',    ['erk','rnap','state'],{'state':['u','p']})
+Monomer('AP1',    ['erk','rnap','state'],{'state':['u','p']})
 
-# Mitochondrial dynamics & quality control
-Monomer('DRP1', ['b'])
-Monomer('MFN1', ['b'])
-Monomer('MFN2', ['b'])
-Monomer('OPA1', ['b'])
-Monomer('PINK1', ['b'])
-Monomer('PRKN', ['b'])
-Monomer('ROS', ['b'])
+# 5) Internalization forms used explicitly in reactions
+Monomer('EGFRi',  ['l','d','grb2','sos','shc','erk'])
+Monomer('EGFi',   [])
 
-# Mitochondrial UPR (UPRmt)
-Monomer('HSPD1', ['b'])
-Monomer('HSPE1', ['b'])
-Monomer('HSPA9', ['b'])
-Monomer('LONP1', ['b'])
-Monomer('CLPP', ['b'])
-Monomer('ATF5', ['b'])
+# 6) PI3K–AKT–mTOR arm
+Monomer('PI3K',   ['egfr_her2','ptdins2','state'],{'state':['i','act']})
+Monomer('PtdIns2',['pi3k'])
+Monomer('PtdIns3',['pten','akt','pdk1'])
+Monomer('PTEN',   ['ptdins3'])
+Monomer('Akt',    ['ptdins3','pdk1','tor','pase7','state'],{'state':['i','m','act']})
+Monomer('Pdk1',   ['ptdins3','akt','state'],{'state':['i','m']})
+Monomer('TOR',    ['akt','e4ebp1','state'],{'state':['i','act']})
+Monomer('E4EBP1', ['eif4e','tor','state'],{'state':['u','P']})
+Monomer('eIF4E',  ['e4ebp1','mrna'])
+Monomer('Pase7',  ['akt'])
 
-# Apoptosis (intrinsic pathway)
-Monomer('CYCS', ['b'])
-Monomer('APAF1', ['b'])
-Monomer('CASP9', ['b'])
-Monomer('CASP3', ['b'])
-Monomer('XIAP', ['b'])
-Monomer('SMAC', ['b'])
+# 7) Transport/secreted forms used explicitly
+Monomer('T_e',    [])
+Monomer('T',      ['rase5a','ar'])
+Monomer('sPAcP_e',[])
 
-# BCL-2 family (anti-apoptotic, effectors, BH3-only)
-Monomer('BCL2', ['bh3'])
-Monomer('BCLXL', ['bh3'])
-Monomer('MCL1', ['bh3'])
-Monomer('BAX', ['b'])
-Monomer('BAK', ['b'])
-Monomer('BIM', ['bh3'])
-Monomer('PUMA', ['bh3'])
-Monomer('NOXA', ['bh3'])
+# 8) AR axis and modifiers
+Monomer('Rase5a', ['T'])
+Monomer('DHT',    ['ar'])
+Monomer('AR',     ['hsp','T','DHT','erk','ar','pase5','state'],{'state':['u','p']})
+Monomer('HSP',    ['ar'])
+Monomer('Pase5',  ['ar','ets','ap1'])
+Monomer('Pase6',  ['ap1'])
+
+# 9) Transcription (promoters/genes) & RNA Pol II
+Monomer('RNAp',   ['promoter'])
+Monomer('g_CycD', ['rnap','ets_p','ap1_p'])
+Monomer('g_PSA',  ['rnap','ar_dimer'])
+Monomer('g_cPAcP',['rnap'])
+Monomer('g_sPAcP',['rnap'])
+
+# 10) mRNAs and translation machinery
+Monomer('mRNA_CycD',['eif4e'])
+Monomer('mRNA_PSA', ['eif4e'])
+Monomer('mRNA_cPAcP',['eif4e'])
+Monomer('mRNA_sPAcP',['eif4e'])
+Monomer('S40', ['mrna'])
+Monomer('S60', ['mrna'])
+
+# 11) Protein products (sinks in your list)
+Monomer('CycD', [])
+Monomer('PSA', [])
+
 
 
 # initial concentrations
@@ -85,6 +93,9 @@ Initial(EGFR(l=None, d=None, state='u'), Rec_0)
 Parameter('kf_LR', 1)
 Parameter('kr_LR', 1)
 Rule('lig_binds_rec', EGF(r=None) + EGFR(l=None, d=None) | EGF(r=1) % EGFR(l=1, d=None), kf_LR, kr_LR)
+Rule('Her2p_binds_cPAcP',Her2(d=ANY, state='p') + cPAcP(her2_olig=None) |
+Her2(d=1, state='p') % cPAcP(her2_olig=1),
+kf_Her2_cPAcP, kr_Her2_cPAcP)
 
 
 # observables
