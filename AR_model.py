5fa15a003ead0439a63e6017b4612964e11b06aa
lh64 <lh64@cornell.edu>
2025-11-11 14:04:03 -0600

    Updated `create_transcription_rules()` in util.py to be more flexible and added more rules to AR_model.py

--- AR_model.py
+++ AR_model.py
@@ -4,7 +4,7 @@ from util import set_model, create_transcription_rules, create_translation_rules
 Model()
 set_model(model)  # put the model into the global namespace
 
-# monomers
+# === MONOMERS ===
 
 # 1) Ligands & Receptors
 Monomer('EGF',  ['r','loc'], {'loc': ['mem','cyt']})
@@ -24,7 +24,7 @@ Monomer('GAP',    ['ras'])
 Monomer('Raf',    ['ras','mek','pase1','state'],{'state':['u','p']})
 Monomer('Pase1',  ['raf'])
 Monomer('MEK',    ['raf','erk','pase2','state'],{'state':['u','p','pp']})
-Monomer('ERK',    ['mek','pase3','sos','ets','ap1','state'],{'state':['u','p','pp']})
+Monomer('ERK',    ['ar', 'mek','pase3','sos','ets','ap1','state'],{'state':['u','p','pp']})
 Monomer('Pase2',  ['mek'])
 Monomer('Pase3',  ['erk'])
 
@@ -32,11 +32,7 @@ Monomer('Pase3',  ['erk'])
 Monomer('ETS',    ['erk','gene','state'],{'state':['u','p']})
 Monomer('AP1',    ['erk','gene','state'],{'state':['u','p']})
 
-# 5) Internalization forms used explicitly in reactions
-# Monomer('EGFRi',  ['l','d','grb2','sos','shc','erk'])
-# Monomer('EGFi')
-
-# 6) PI3K–AKT–mTOR arm
+# 5) PI3K–AKT–mTOR arm
 Monomer('PI3K',   ['egfr_her2','ptdins2','sos','state'],{'state':['i','act']})
 Monomer('PtdIns2',['pi3k'])  # PIP2
 Monomer('PtdIns3',['pten','akt','pdk1'])  # PIP3
@@ -47,27 +43,23 @@ Monomer('TOR',    ['akt','e4ebp1','state'],{'state':['i','act']})
 Monomer('_4EBP1', ['eif4e','tor','state'],{'state':['u','p']})
 Monomer('Pase7',  ['akt'])
 
-# 7) Transport/secreted forms used explicitly
-# Monomer('T_e')
-# Monomer('sPAcP_e')
-
-# 8) AR axis and modifiers
-Monomer('AR',     ['hsp','t','DHT','erk','ar','pase5','state'],{'state':['u','p']})
-Monomer('T',      ['rase5a','ar','loc'], {'loc': ['intra', 'extra']})
+# 6) AR axis and modifiers
+Monomer('AR',     ['lig','ar','erk','pase5','gene','state'], {'state':['u','p']})
+Monomer('T',      ['b','loc'], {'loc': ['intra', 'extra']})  # testosterone
 Monomer('Rase5a', ['t'])
 Monomer('HSP',    ['ar'])
-Monomer('DHT',    ['ar'])
+Monomer('DHT',    ['b'])  # dihydrotestosterone
 Monomer('Pase5',  ['ar','ets','ap1'])
 Monomer('Pase6',  ['ap1'])
 
-# 9) Transcription (promoters/genes) & RNA Pol II
+# 7) Transcription (promoters/genes) & RNA Pol II
 Monomer('RNAp',   ['gene'])
-Monomer('g_cPAcP',['rnap','tf'])
+Monomer('g_cPAcP',['rnap','tf1','tf2'])
 Monomer('g_sPAcP',['rnap','tf'])
 Monomer('g_CycD', ['rnap','tf'])
 Monomer('g_PSA',  ['rnap','tf'])
 
-# 10) mRNAs and translation machinery
+# 8) mRNAs and translation machinery
 Monomer('eIF4E',     ['mrna_4ebp1'])
 Monomer('_40S',      ['mrna', '_60s'])
 Monomer('_60S',      ['_40s'])
@@ -76,30 +68,31 @@ Monomer('mRNA_sPAcP',['eif4e','_40s','elong'], {'elong': ['i','a']})
 Monomer('mRNA_CycD', ['eif4e','_40s','elong'], {'elong': ['i','a']})
 Monomer('mRNA_PSA',  ['eif4e','_40s','elong'], {'elong': ['i','a']})
 
-# 11) Protein products (sinks in your list)
+# 9) Protein products (sinks in your list)
 Monomer('CycD')
 Monomer('PSA')
 
+# === INITIAL CONCENTRATIONS ===
 
-# initial concentrations
 Parameter('Lig_0', 100)
 Parameter('Rec_0', 100)
 
 Initial(EGF(r=None, loc='mem'), Lig_0)
 Initial(EGFR(l=None, d=None, grb2_shc=None, state='u', loc='mem'), Rec_0)
 
-
-# rules
+# === RULES ===
 
 # CycD transcription
-tfs = [ETS(erk=None,gene=None,state='p'), AP1(erk=None,gene=None,state='p')]
-kf_kr_kcat = [
-    [4.952e-5, 0.1104, 1.099e-2],  # basal
-    [0.2931, 9.037e-3, 1.156e-2],  # ETS-p
-    [0.4288, 2.945e-2, 3.292e-2]]  # AP1-p
+kf_kr_kcat = [[4.952e-5, 0.1104, 1.099e-2]]  # basal
+k_mRNA_deg = 0.8094
+tf_species = [ETS(erk=None,gene=None,state='p'), AP1(erk=None,gene=None,state='p')]
+tf_names = ['ETS_p', 'AP1_p']
 k_tf_on_off = [[0.138, 1.26], [0.3726, 2.171]]  # ETS-p, AP1-p
-k_deg = 0.8094
-create_transcription_rules(CycD, kf_kr_kcat, k_deg, tfs=tfs, k_tf_on_off=k_tf_on_off)
+kf_kr_kcat.extend(
+    [[0.2931, 9.037e-3, 1.156e-2],  # ETS-p
+    [0.4288, 2.945e-2, 3.292e-2]]   # AP1-p
+)
+create_transcription_rules(CycD, kf_kr_kcat, k_mRNA_deg, tfs=list(zip(tf_species, tf_names)), k_tf_on_off=k_tf_on_off)
 
 # CycD translation
 kf_kr = [
@@ -110,12 +103,177 @@ kf_kr = [
 k_release = 1.194
 k_elongate = 0.9399
 k_terminate = 1.781
-k_deg = 6.123e-3
-create_translation_rules(CycD, kf_kr, k_release, k_elongate, k_terminate, k_deg)
+k_prot_deg = 6.123e-3
+create_translation_rules(CycD, kf_kr, k_release, k_elongate, k_terminate, k_prot_deg)
 
+# PSA transcription
+# ... TODO
 
-# Add other transcription and translation rules
-# ...
+# PSA translation
+# ... TODO
+
+# cPAcP transcription
+kf_kr_kcat = [[5.788e-2, 7.556e-2, 1.311e-2]]  # basal
+k_mRNA_deg = 1.3
+tf_species = []
+tf_names = []
+k_tf_on_off = []
+# AR-p-2 + g-cPAcP <-> AR-p-2-g-cPAcP
+tf_species.append(
+    AR(lig=None, ar=1, erk=None, pase5=None, gene=None, state='p') %
+    AR(lig=None, ar=1, erk=None, pase5=None, gene=None, state='p'))
+tf_names.append('AR_p_2')
+k_tf_on_off.append([1.618e-3, 9.872e-5])
+# AR-p-DHT-2 + g-cPAcP <-> AR-p-DHT-2-g-cPAcP
+tf_species.append(
+    DHT(b=2) % AR(lig=2, ar=1, erk=None, pase5=None, gene=None, state='p') %
+    DHT(b=3) % AR(lig=3, ar=1, erk=None, pase5=None, gene=None, state='p'))
+tf_names.append('AR_p_DHT_2')
+k_tf_on_off.append([8.423e-4, 1.366e-7])
+# AR-p-DHT-AR-p + g-cPAcP <-> AR-p-DHT-AR-p-g-cPAcP
+tf_species.append(
+    DHT(b=2) % AR(lig=2, ar=1, erk=None, pase5=None, gene=None, state='p') %
+    AR(lig=None, ar=1, erk=None, pase5=None, gene=None, state='p'))
+tf_names.append('AR_p_DHT_AR_p')
+k_tf_on_off.append([4.544e-3, 1.314e-7])
+# AR-p-T-2 + g-cPAcP <-> AR-p-T-2-g-cPAcP
+tf_species.append(
+    T(b=2, rase5a=None, loc='intra') % AR(lig=2, ar=1, erk=None, pase5=None, gene=None, state='p') %
+    T(b=3, rase5a=None, loc='intra') % AR(lig=3, ar=1, erk=None, pase5=None, gene=None, state='p'))
+tf_names.append('AR_p_T_2')
+k_tf_on_off.append([6.374e-2, 2.954e-4])
+# AR-p-T-AR-p + g-cPAcP <-> AR-p-T-AR-p-g-cPAcP
+tf_species.append(
+    T(b=2, rase5a=None, loc='intra') % AR(lig=2, ar=1, erk=None, pase5=None, gene=None, state='p') %
+    AR(lig=None, ar=1, erk=None, pase5=None, gene=None, state='p'))
+tf_names.append('AR_p_T_AR_p')
+k_tf_on_off.append([6.191e-2, 1.099e-4])
+# AR-p-DHT-AR-p-T + g-cPAcP <-> AR-p-DHT-AR-p-T-g-cPAcP
+tf_species.append(
+    DHT(b=2) % AR(lig=2, ar=1, erk=None, pase5=None, gene=None, state='p') %
+    T(b=3, rase5a=None, loc='intra') % AR(lig=3, ar=1, erk=None, pase5=None, gene=None, state='p'))
+tf_names.append('AR_p_DHT_AR_p_T')
+k_tf_on_off.append([0.1543,   2.343e-4])
+create_transcription_rules(cPAcP, kf_kr_kcat, k_mRNA_deg, tfs=list(zip(tf_species, tf_names)), k_tf_on_off=k_tf_on_off)
+
+# cPAcP translation
+kf_kr = [
+    [1.351e-2, 9.892e-3],  # eIF4E
+    [0.233, 9.908e-3],     # 40S
+    [0.9053, 1.499e-3]     # 60S
+]
+k_release = 1.185
+k_elongate = 1.31
+k_terminate = 1.612
+k_prot_deg = 1.208e-2
+create_translation_rules(cPAcP, kf_kr, k_release, k_elongate, k_terminate, k_prot_deg)
+
+# sPAcP transcription
+# ... TODO
+
+# sPAcP translation
+# ... TODO
+
+# AR+ERK-pp	↔	AR-ERK-pp
+# AR-ERK-pp	↔	AR-p+ERK-pp
+Parameter('kf_ERKpp_phos_AR', 1.873e-3)
+Parameter('kr_AERKpp_phos_AR', 0.388)
+Parameter('kcat_ERKpp_phos_AR', 2.57E-2)
+Rule('AR_binds_ERKpp',
+     AR(lig=None, ar=None, erk=None, pase5=None, state='u') +
+     ERK(ar=None, mek=None, pase3=None, sos=None, ets=None, ap1=None, state='pp') |
+     AR(lig=None, ar=None, erk=1, pase5=None, state='u') %
+     ERK(ar=1, mek=None, pase3=None, sos=None, ets=None, ap1=None, state='pp'),
+     kf_ERKpp_phos_AR, kr_AERKpp_phos_AR)
+Rule('AR_ERKpp_phosphorylates_AR',
+     AR(lig=None, ar=None, erk=1, pase5=None, state='u') %
+     ERK(ar=1, mek=None, pase3=None, sos=None, ets=None, ap1=None, state='pp') >>
+     AR(lig=None, ar=None, erk=None, pase5=None, state='p') +
+     ERK(ar=None, mek=None, pase3=None, sos=None, ets=None, ap1=None, state='pp'),
+     kcat_ERKpp_phos_AR)
+
+# T+Rase5a	↔	T-Rase5a
+# T-Rase5a	→	DHT+Rase5a
+Parameter('kf_T_Rase5a', 2.171e-2)
+Parameter('kr_T_Rase5a', 0.3308)
+Parameter('kcat_T_Rase5a', 1.374)
+Rule('T_binds_Rase5a',
+     T(b=None, loc='intra') + Rase5a(t=None) | T(b=1, loc='intra') % Rase5a(t=1), kf_T_Rase5a, kr_T_Rase5a)
+Rule('T_converts_to_DHT', T(b=1, loc='intra') % Rase5a(t=1) >> DHT(b=None) + Rase5a(t=None), kcat_T_Rase5a)
+
+# AR+HSP	↔	AR-HSP
+Parameter('kf_AR_binds_HSP', 9.162e-3)
+Parameter('kr_AR_binds_HSP', 1.01e-3)
+Rule('AR_binds_HSP',
+     AR(lig=None, ar=None, erk=None, pase5=None, state='u') + HSP(ar=None) |
+     AR(lig=1, ar=None, erk=None, pase5=None, state='u') % HSP(ar=1),
+     kf_AR_binds_HSP, kr_AR_binds_HSP)
+
+# AR+T	↔	AR-T
+# AR-T	→	AR-p-T
+Parameter('kf_AR_binds_T', 5.289e-3)
+Parameter('kr_AR_binds_T', 9.361e-4)
+Parameter('kcat_AR_binds_T', 5.458E-2)
+Rule('AR_binds_T',
+     AR(lig=None, ar=None, erk=None, pase5=None, gene=None, state='u') + T(b=None) |
+     AR(lig=1, ar=None, erk=None, pase5=None, gene=None, state='u') % T(b=1),
+     kf_AR_binds_T, kr_AR_binds_T)
+Rule('AR_T_phosphorylates',
+     AR(lig=1, ar=None, erk=None, pase5=None, gene=None, state='u') % T(b=1) >>
+     AR(lig=1, ar=None, erk=None, pase5=None, gene=None, state='p') % T(b=1), kcat_AR_binds_T)
+
+# AR-p+AR-p-T	↔	AR-p-AR-p-T
+Parameter('kon_AR_p_AR_p_T', 0.532)
+Parameter('koff_AR_p_AR_p_T', 6.128e-4)
+Rule('AR_p_binds_AR_p_T',
+     AR(lig=None, ar=None, erk=None, pase5=None, gene=None, state='p') +
+     AR(lig=1, ar=None, erk=None, pase5=None, gene=None, state='p') % T(b=1) |
+     AR(lig=None, ar=2, erk=None, pase5=None, gene=None, state='p') +
+     AR(lig=1, ar=2, erk=None, pase5=None, gene=None, state='p') % T(b=1),
+     kon_AR_p_AR_p_T, koff_AR_p_AR_p_T)
+
+# 2*AR-p-T	↔	AR-p-T-2
+Parameter('kon_AR_p_T_dimerize', 0.5835)
+Parameter('koff_AR_p_T_dimerize', 5.149e-4)
+Rule('AR_p_T_dimerizes',
+     AR(lig=1, ar=None, erk=None, pase5=None, gene=None, state='p') % T(b=1) +
+     AR(lig=1, ar=None, erk=None, pase5=None, gene=None, state='p') % T(b=1) |
+     AR(lig=1, ar=2, erk=None, pase5=None, gene=None, state='p') % T(b=1) +
+     AR(lig=1, ar=2, erk=None, pase5=None, gene=None, state='p') % T(b=1),
+     kon_AR_p_T_dimerize, koff_AR_p_T_dimerize)
+
+# 2*AR-p	↔	AR-p-2
+Parameter('kon_AR_p_dimerize', 0.2848)
+Parameter('koff_AR_p_dimerize', 0.1281)
+Rule('AR_p_dimerizes',
+     AR(lig=None, ar=None, erk=None, pase5=None, gene=None, state='p') +
+     AR(lig=None, ar=None, erk=None, pase5=None, gene=None, state='p') |
+     AR(lig=None, ar=1, erk=None, pase5=None, gene=None, state='p') +
+     AR(lig=None, ar=1, erk=None, pase5=None, gene=None, state='p'),
+     kon_AR_p_dimerize, koff_AR_p_dimerize)
+
+# AR+DHT	↔	AR-DHT
+# AR-DHT	→	AR-p-DHT
+
+# AR-p-DHT+AR-p-T	↔	AR-p-DHT-AR-p-T
+
+# AR-p-DHT+AR-p	↔	AR-p-DHT-AR-p
+
+# 2*AR-p-DHT	↔	AR-p-DHT-2
+
+# AR-p+Pase5	↔	AR-p-Pase5
+# AR-p-Pase5	→	AR+Pase5
+
+# AR-p-T+Pase5	↔	AR-p-T-Pase5
+# AR-p-T-Pase5	→	AR-T+Pase5
+
+# AR-p-DHT+Pase5	↔	AR-p-DHT-Pase5
+# AR-p-DHT-Pase5	→	AR-DHT+Pase5
+
+# T-e	↔	T
+Parameter('kf_T_extra_to_intra', 1.449)
+Parameter('kr_T_extra_to_intra', 1.555)
+Rule('T_extra_to_intra', T(b=None, loc='extra') | T(b=None, loc='intra'), kf_T_extra_to_intra, kr_T_extra_intra)
 
 
 Parameter('kf_LR', 1)
--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 from pysb import *
-from pysb import MonomerPattern
+from pysb import MonomerPattern, ComplexPattern
 from pysb.util import alias_model_components
 import numpy as np
 
@@ -36,35 +36,52 @@ def create_transcription_rules(prot_monomer, kf_kr_kcat, k_deg, tfs=None, k_tf_o
     # RNAp binds to gene and transcribes mRNA
     kf, kr, kcat = \
         [Parameter('%s_%s_RNAp' % (k, gene.name), kf_kr_kcat[0][i]) for i, k in enumerate(['kf', 'kr', 'kcat'])]
+    tf_sites = [site for site in gene.sites if 'tf' in site]
+    tf_sites_dict = dict(list(zip(tf_sites, [None] * len(tf_sites))))
     Rule('%s_binds_RNAp' % gene.name,
-         gene(rnap=None, tf=None) + RNAp(gene=None) |
-         gene(rnap=1, tf=None) % RNAp(gene=1), kf, kr)
+         gene(rnap=None, **tf_sites_dict) + RNAp(gene=None) |
+         gene(rnap=1, **tf_sites_dict) % RNAp(gene=1), kf, kr)
     Rule('%s_RNAp_transcribes' % gene.name,
-         gene(rnap=1, tf=None) % RNAp(gene=1) >>
-         gene(rnap=1, tf=None) % RNAp(gene=1) + mrna(eif4e=None,_40s=None,elong='i'), kcat)
+         gene(rnap=1, **tf_sites_dict) % RNAp(gene=1) >>
+         gene(rnap=1, **tf_sites_dict) % RNAp(gene=1) + mrna(eif4e=None,_40s=None,elong='i'), kcat)
 
     # mRNA degradation
     k_deg = Parameter('kdeg_%s' % mrna.name, k_deg)
     Rule('%s_degrades' % mrna.name, mrna(eif4e=None,_40s=None,elong='i') >> None, k_deg)
 
     if tfs is not None:
-        for i, tf in enumerate(tfs):
+        for i, (tf, tf_name) in enumerate(tfs):
             # transcription factor (TF) binds to gene
-            k_on, k_off = [Parameter('%s_%s_%s' % (k, gene.name, tf.monomer.name), k_tf_on_off[i][j])
+            k_on, k_off = [Parameter('%s_%s_%s' % (k, gene.name, tf_name), k_tf_on_off[i][j])
                            for j, k in enumerate(['kon', 'koff'])]
-            Rule('%s_binds_%s' % (gene.name, tf.monomer.name),
-                 gene(rnap=None, tf=None) + tf(gene=None) |
-                 gene(rnap=None, tf=1) % tf(gene=1), k_on, k_off)
+            if isinstance(tf, MonomerPattern):
+                gene_unbound = gene(rnap=None, tf=None)
+                gene_bound = gene(rnap=None, tf=50)
+                tf_bound = tf(gene=50)
+            elif isinstance(tf, ComplexPattern):
+                site_conditions = dict(
+                    [('rnap', None)] + [('tf%d' % (i + 1), None) for i in range(len(gene.sites) - 1)])
+                gene_unbound = MonomerPattern(gene, site_conditions, None)
+                site_conditions = dict(
+                    [('rnap', None)] + [('tf%d' % (i + 1), 50 + i) for i in range(len(gene.sites) - 1)])
+                gene_bound = MonomerPattern(gene, site_conditions, None)
+                mp_gene_sites = [mp for mp in tf.monomer_patterns if 'gene' in mp.monomer.sites]
+                tf_bound = ComplexPattern([mp(gene=50 + i) for i, mp in enumerate(mp_gene_sites)] +
+                                          [mp for mp in tf.monomer_patterns if mp not in mp_gene_sites],
+                                          None)
+            Rule('%s_binds_%s' % (gene.name, tf_name),
+                 gene_unbound + tf | gene_bound % tf_bound, k_on, k_off)
 
             # RNAp binds to TF-bound gene and transcribes mRNA
-            kf, kr, kcat = [Parameter('%s_%s_%s_RNAp' % (k, gene.name, tf.monomer.name), kf_kr_kcat[i+1][j])
-                                      for j, k in enumerate(['kf', 'kr', 'kcat'])]
-            Rule('%s_%s_binds_RNAp' % (gene.name, tf.monomer.name),
-                 gene(rnap=None, tf=1) % tf(gene=1) + RNAp(gene=None) |
-                 gene(rnap=2, tf=1) % tf(gene=1) % RNAp(gene=2), kf, kr)
-            Rule('%s_%s_RNAp_transcribes' % (gene.name, tf.monomer.name),
-                 gene(rnap=2, tf=1) % tf(gene=1) % RNAp(gene=2) >>
-                 gene(rnap=2, tf=1) % tf(gene=1) % RNAp(gene=2) + mrna(eif4e=None, _40s=None,elong='i'), kcat)
+            if len(kf_kr_kcat) > i + 1:
+                kf, kr, kcat = [Parameter('%s_%s_%s_RNAp' % (k, gene.name, tf_name), kf_kr_kcat[i+1][j])
+                                          for j, k in enumerate(['kf', 'kr', 'kcat'])]
+                Rule('%s_%s_binds_RNAp' % (gene.name, tf_name),
+                     gene(rnap=None, tf=50) % tf_bound + RNAp(gene=None) |
+                     gene(rnap=2, tf=50) % tf_bound % RNAp(gene=2), kf, kr)
+                Rule('%s_%s_RNAp_transcribes' % (gene.name, tf_name),
+                     gene(rnap=2, tf=50) % tf_bound % RNAp(gene=2) >>
+                     gene(rnap=2, tf=50) % tf_bound % RNAp(gene=2) + mrna(eif4e=None, _40s=None,elong='i'), kcat)
 
 
 def create_translation_rules(prot_monomer, kf_kr, k_release, k_elongate, k_terminate, k_deg):
